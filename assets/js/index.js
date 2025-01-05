cabde2533f8256fb146370248b3065892edcc92f
byeon-seong-won <sngoneee@gamil.com>
2025-01-05 17:58:13 +0900

    수정

--- assets/js/index.js
+++ assets/js/index.js
@@ -66,6 +66,11 @@ import '../img/freitag-03.webp';
 import '../img/bbt-01.webp';
 import '../img/bbt-02.webp';
 import '../img/bbt-03.webp';
+import '../img/wt-01.webp';
+import '../img/wt-02.webp';
+import '../img/wt-03.webp';
+import '../img/dumcbd.webp';
+import '../img/dumcbd-02.webp';
 
 
 
